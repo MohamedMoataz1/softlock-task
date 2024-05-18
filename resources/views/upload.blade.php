e877be78d10bb97220c9f0462050a647540ebbbd
moataz99999 <89107303+moataz99999@users.noreply.github.com>
2024-05-18 11:00:55 +0300

    feat: add CSS and JS files, update Blade template to include them

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -5,68 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        .input {
-            padding: 20px 10px;
-            border-radius: 10px;
-            border: 2px solid grey;
-            /* width: 60%; */
-            cursor: pointer;
-
-        }
-        
-
-        .input-holder {
-            width: 100%;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            gap: 20px;
-        }
-
-        .submit {
-            padding: 10px 15px;
-            border: none;
-            border-radius: 10px;
-            cursor: pointer;
-        }
-
-        .container {
-            height: 100vh;
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-        }
-
-        .buttons {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-            gap: 10px;
-        }
-
-        .buttons input {
-            padding: 10px 15px;
-            cursor: pointer;
-            border: none;
-            border-radius: 10px;
-            background-color: blue;
-            color: aliceblue;
-        }
-
-        .details {
-            display: none;
-        }
-
-        .confirmation {
-            display: none;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 
 <body>
@@ -91,24 +30,6 @@
 
 </body>
 
-<script>
-    function fileDetailsDisplay(input) {
-        let file = input.files[0];
-        let name = file.name.split('.')
-        let detailsDiv = document.getElementsByClassName('details')[0];
-        detailsDiv.style.display = "block";
-        document.getElementById('file-name').innerHTML = "file name: " + name[0];
-        document.getElementById('file-size').innerHTML = "file size : " + file.size + " bytes    ";
-        document.getElementById('file-extension').innerHTML = "file extension : " + "." + name[1];
-
-
-    }
-
-    function confirmation() {
-
-        let confirmationSection = document.getElementsByClassName(confirmation)[0];
-        confirmationSection.style.display = "block";
-    }
-</script>
+<script src="{{ asset('js/scripts.js') }}"></script>
 
 </html>
